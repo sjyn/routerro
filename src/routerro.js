17f40dd0836f045210d36f76382fefd968f5a167
Steven Rosendahl <steven.ak.rosendahl@gmail.com>
2019-08-01 18:42:11 -0600

    yeet

--- src/routerro.js
+++ src/routerro.js
@@ -1,5 +1,6 @@
 class Router {
   constructor(contentHolderId) {
+    this.onHashChange = undefined;
     this.routes = [];
     this.contentHolderId = contentHolderId;
     this._obeyHash = true;
@@ -10,11 +11,11 @@ class Router {
     this.routes.push(route);
   }
 
-  navigateToRoute(routeId) {
+  navigateToRoute(routeId, switchCallback) {
     window.location.hash = routeId;
     const route = this.routes.find((r) => r.id === routeId);
     if (!!route) {
-      this._switchContent(route);
+      this._switchContent(route, switchCallback);
     } else {
       throw new Error(`Route id ${routeId} is not a registered route`);
     }
@@ -25,20 +26,27 @@ class Router {
       if (this._obeyHash) {
         const hash = window.location.hash.substr(1);
         try {
+          // noinspection JSPotentiallyInvalidUsageOfClassThis
           this.navigateToRoute(hash);
         } catch (ignored) {
 
         }
       }
+      if (!!this.onHashChange) {
+        this.onHashChange();
+      }
     }.bind(this);
   }
 
-  _switchContent(route) {
+  _switchContent(route, switchCallback) {
     const {path} = route;
     const request = new XMLHttpRequest();
     const contentId = this.contentHolderId;
     request.onload = () => {
       document.getElementById(contentId).innerHTML = request.response;
+      if (!!switchCallback) {
+        switchCallback();
+      }
     };
     request.open('GET', path);
     request.send()
